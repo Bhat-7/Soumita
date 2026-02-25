79b448b238ceeeb457f21d45663995585a6e5583
Debayan Sen <debayan.sen7@gmail.com>
2026-02-25 13:28:59 +0530

    update css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,9 +4,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <h1>Hi! Soumita Bhattacharya Sen here</h1>
-      <Avatar profileImage="https://media.licdn.com/dms/image/v2/D5603AQFYQ9zZtVOQAQ/profile-displayphoto-scale_200_200/B56ZhFj32dG0Ak-/0/1753513693047?e=1773878400&v=beta&t=07F_TWYIob1Baorud555_Un8fqCKY9q_fFR00hbfRSE" />
-      <p></p>
+      <div className="home-content">
+        <h1>Hi! Soumita Bhattacharya Sen</h1>{" "}
+        <Avatar profileImage="https://media.licdn.com/dms/image/v2/D5603AQFYQ9zZtVOQAQ/profile-displayphoto-scale_200_200/B56ZhFj32dG0Ak-/0/1753513693047?e=1773878400&v=beta&t=07F_TWYIob1Baorud555_Un8fqCKY9q_fFR00hbfRSE" />
+        <p>
+          I'm a passionate data analyst with expertise in data visualization,
+          statistical analysis, and machine learning.
+        </p>
+      </div>
     </div>
   );
 };
